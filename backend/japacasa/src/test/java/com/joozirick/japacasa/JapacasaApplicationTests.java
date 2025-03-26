42c847a3c5b96ff6c2d16ac16637a541d1e3607a
Ja1zinh0 <joao2011bha@hotmail.com>
2025-03-26 07:37:26 -0300

    fix: backend files

---
+++ backend/japacasa/src/test/java/com/joozirick/japacasa/JapacasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joozirick.japacasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapacasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
