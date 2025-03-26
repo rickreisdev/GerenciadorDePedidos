83e6e0ea76126b8187a2059f763c5bf4be70de5d
Ja1zinh0 <joao2011bha@hotmail.com>
2025-03-26 07:03:48 -0300

    add: backend folder

---
+++ backend/japacasa/src/main/java/com/joozirick/japacasa/JapacasaApplication.java
@@ -0,0 +1,13 @@
+package com.joozirick.japacasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapacasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapacasaApplication.class, args);
+	}
+
+}
